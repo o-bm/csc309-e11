18b508b71cfc2fe107c002ad9609a3ee54a84769
okhamis <omar.khamis@mail.utoronto.ca>
2025-03-31 05:48:26 -0400

    d

--- my-app/src/app/csc309-e11/page.js
+++ my-app/src/app/csc309-e11/page.js
@@ -1,4 +1,10 @@
-<div id="auto-tester">
-  Hi. I am khamisom, and I have learned how to develop a full-stack web app
-  and deploy it!
-</div>
+'use client';
+
+export default function Page() {
+  return (
+    <div id="auto-tester">
+      Hi. I am khamisom, and I have learned how to develop a full-stack web app
+      and deploy it!
+    </div>
+  );
+}
